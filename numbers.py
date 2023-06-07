0ee54b95410e36113fca6e1d973e14c2bf783495
Artem Bakanov <rtmbakanov@gmail.com>
2023-06-07 21:32:43 +0300

    add numbers.py on develop

---
+++ numbers.py
@@ -0,0 +1,20 @@
+print("Write four numbers: ")
+a = input()
+a = int(a)
+
+b = input()
+b = int(b)
+
+c = input()
+c = int(c)
+
+d = input()
+d = int(d)
+
+q = a + b
+w = c + d
+e = q / w
+e = float(e)
+
+print("Results: %.2f" % e)
+input('Press ENTER to exit')
