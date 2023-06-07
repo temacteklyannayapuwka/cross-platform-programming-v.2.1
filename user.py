142549d03c9ad67106974bd50d3a31275764b0a3
Artem Bakanov <rtmbakanov@gmail.com>
2023-06-07 21:32:00 +0300

    add user.py on develop

---
+++ user.py
@@ -0,0 +1,5 @@
+name = input("Write our name: ")
+old = input("How old are you: ")
+town = input("Where do you live: ")
+print("Your name is: {0}. \nYour age: {1}. \nYour town is: {2}. \n".format(name, old, town))
+input("Press ENTER to exit")
